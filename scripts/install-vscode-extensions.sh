a9ac0daea0a434459646b013cf4b6df258a4db8b
GoYamada <tama_jp@me.com>
2022-05-12 19:17:28 +0900

    shellの修正 #や空白はスキップできるように

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -3,12 +3,15 @@
 echo install VSCode extension
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-  if ${#line::1} > 0; then
-    if [[ "${line::1}" != "#" ]]; then
-      echo "Installing $line using VSCode";
+  if [ ${#line} -gt 0 ]; then
+    if [[ ${line:0:1} != "#" ]]; then
+      echo "install $line";
       /works/code-server --user-data-dir /root/.local/share/code-server --install-extension $line
+    else
+      echo "Skip $line";
     fi
+
   else
-    echo "Skip $line using VSCode";
+    echo "Skip $line";
   fi
 done < "$1"
